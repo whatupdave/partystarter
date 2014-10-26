152d08a7f6ccbcd81559a71374f865e04068bcee
Dave Newman <dave@snappyco.de>
2014-10-26 16:50:26 -0700

    Let's run a tekkit server!

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "subdomain" {
-  default = "mc"
-}
+
 variable "do_token" {}
 variable "do_region" {
   default = "sfo1"
@@ -16,7 +14,6 @@ variable "dnsimple_email" {}
 variable "key_file" {
   default = "~/.ssh/id_rsa"
 }
-variable "s3_path" {}
 variable "ssh_fingerprint" {}
 
 provider "digitalocean" {
@@ -27,9 +24,53 @@ provider "dnsimple" {
     email = "${var.dnsimple_email}"
 }
 
-resource "digitalocean_droplet" "node" {
+variable "vanilla_s3_path" {}
+variable "vanilla_subdomain" {
+  default = "vanilla"
+}
+
+variable "tekkit_s3_path" {}
+variable "tekkit_subdomain" {
+  default = "tekkit"
+}
+
+# --- Vanilla server
+
+# resource "digitalocean_droplet" "vanilla" {
+#   image = "docker"
+#   name = "vanilla"
+#   region = "${var.do_region}"
+#   size = "${var.do_size}"
+#   ssh_keys = ["${var.ssh_fingerprint}"]
+#
+#   connection {
+#     user = "root"
+#     type = "ssh"
+#     key_file = "${var.key_file}"
+#     timeout = "2m"
+#   }
+#
+#   provisioner "remote-exec" {
+#     inline = [
+#       "docker run -d -e ACCESS_KEY=${var.aws_access_key} -e SECRET_KEY=${var.aws_secret_key} --name data whatupdave/s3-volume ${var.vanilla_s3_path}",
+#       "while ! docker logs data | grep 'ready'; do sleep 1; done",
+#       "docker run -id -p 25565:25565 --volumes-from data --name minecraft whatupdave/partycloud-minecraft"
+#     ]
+#   }
+# }
+# resource "dnsimple_record" "vanilla_subdomain" {
+#   domain = "${var.domain}"
+#   name = "${var.vanilla_subdomain}"
+#   value = "${digitalocean_droplet.vanilla.ipv4_address}"
+#   type = "A"
+#   ttl = 60
+# }
+
+# --- Tekkit server
+
+resource "digitalocean_droplet" "tekkit" {
   image = "docker"
-  name = "node"
+  name = "tekkit"
   region = "${var.do_region}"
   size = "${var.do_size}"
   ssh_keys = ["${var.ssh_fingerprint}"]
@@ -43,21 +84,16 @@ resource "digitalocean_droplet" "node" {
 
   provisioner "remote-exec" {
     inline = [
-      "docker run -d -e ACCESS_KEY=${var.aws_access_key} -e SECRET_KEY=${var.aws_secret_key} --name data whatupdave/s3-volume ${var.s3_path}",
+      "docker run -d --restart=always -e ACCESS_KEY=${var.aws_access_key} -e SECRET_KEY=${var.aws_secret_key} --name data whatupdave/s3-volume ${var.tekkit_s3_path}",
       "while ! docker logs data | grep 'ready'; do sleep 1; done",
-      "docker run -id -p 25565:25565 --volumes-from data --name minecraft whatupdave/partycloud-minecraft"
+      "docker run -id -p 25565:25565 --restart=always --volumes-from data --name minecraft whatupdave/partycloud-minecraft:tekkit"
     ]
   }
 }
-
-resource "dnsimple_record" "subdomain" {
+resource "dnsimple_record" "tekkit_subdomain" {
   domain = "${var.domain}"
-  name = "${var.subdomain}"
-  value = "${digitalocean_droplet.node.ipv4_address}"
+  name = "${var.tekkit_subdomain}"
+  value = "${digitalocean_droplet.tekkit.ipv4_address}"
   type = "A"
-  ttl = 60
-}
-
-output "server" {
-  value = "${var.subdomain}.${var.domain}"
+  ttl = 30
 }
